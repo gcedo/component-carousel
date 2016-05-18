f52b273acdad71e6d1bf1b22929a15a4c8178edd
Edoardo Colombo <edoardocolombo@economist.com>
2016-05-18 12:24:16 +0100

    test: add tests

--- test/index.js
+++ test/index.js
@@ -4,8 +4,9 @@ import { horizontalNodes } from '../src/example';
 import React from 'react';
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
+import spies from 'chai-spies';
 import { mount } from 'enzyme';
-chai.use(chaiEnzyme()).should();
+chai.use(chaiEnzyme()).use(spies).should();
 describe('Carousel', () => {
 
   it('renders a React element', () => {
@@ -34,11 +35,47 @@ describe('Carousel', () => {
       carousel.should.have.className('carousel');
     });
 
-    xit('renders <FILL THIS IN>', () => {
-      carousel.should.have.exactly(1).descendants('.the-descendent-class');
-      carousel.find('.the-descendent-class').should.have.tagName('TAG');
+    it('renders correctly the list of elements', () => {
+      carousel.should.have.exactly(1).descendants('.carousel__list');
+      carousel.find('.carousel__list').should.have.exactly(5).descendants('.carousel__item');
+      carousel.should.have.exactly(1).descendants('.carousel__control--next');
+      carousel.should.have.exactly(1).descendants('.carousel__control--previous');
     });
 
+    it('computes the correct dimensions', () => {
+      const carouselList = carousel.find('.carousel__list');
+      carouselList.children().forEach((carouselItem) => {
+        carouselItem.should.have.style('paddingRight', '11.25px');
+        carouselItem.should.have.style('paddingLeft', '11.25px');
+      });
+      carouselList.should.have.style('width', '28.125px');
+      carouselList.should.have.style('marginLeft', '-11.25px');
+      carouselList.should.have.style('marginRight', '-11.25px');
+      carouselList.children().forEach((carouselItem) => {
+        carouselItem.should.have.style('width', '5.625px');
+      });
+    });
+
+    it('displays the correct controls', () => {
+      const controlNext = carousel.find('.carousel__control--next');
+      const controlPrevious = carousel.find('.carousel__control--previous');
+      controlNext.find('span').should.have.text('▶');
+      controlPrevious.find('span').should.have.text('◀');
+    });
+
+    it('responds correctly to nextButton click', () => {
+      const spiedHandleNextClick = chai.spy(rendered.instance().handleNextClick);
+      rendered.instance().handleNextClick = spiedHandleNextClick;
+      carousel.find('.carousel__control--next').simulate('click');
+      spiedHandleNextClick.should.have.been.called.exactly(1);
+    });
+
+    it('responds correctly to previousButton click', () => {
+      const spiedHandlePreviousClick = chai.spy(rendered.instance().handlePreviousClick);
+      rendered.instance().handlePreviousClick = spiedHandlePreviousClick;
+      carousel.find('.carousel__control--previous').simulate('click');
+      spiedHandlePreviousClick.should.have.been.called.exactly(1);
+    });
   });
 
 });
